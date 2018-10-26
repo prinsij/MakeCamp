939c6077ae7b108b72c26c00986917e7d9c81725
Chamu Rajasekera <chamu.rajasekera@gmail.com>
2018-10-25 23:34:28 -0400

    complete v1.0

--- make_camp.js
+++ make_camp.js
@@ -1,22 +1,25 @@
 'use strict';
-(function () {
-    const EMPTY_ROOMS = 0;
-    const ALL_ROOMS = 1;
-    let App = {
+(function() {
+    const App = {
         day: null,
         day_dropdown: '#input-day',
         time: null,
         time_dropdown: '#input-time-start',
         duration: null,
         duration_dropdown: '#input-time-length',
-        room_type: EMPTY_ROOMS,
-        room_type_dropdown: '#input-empty-only'
+        room_type: "Empty",
+        room_type_dropdown: '#input-empty-only',
+        submit_button: '#submit-data',
+        building_input: '#input-building',
+        output_container: '#request-output',
     };
+    App.update_value = function($item, key) {
+        App[key] = $item.data('value');
+        $item.parent().parent().children('.dropdown-toggle').text($item.text());
+    }
     App.hook = function(element, key) {
         $(element).find('.dropdown-menu > .dropdown-item').click(function() {
-            let $this = $(this);
-            App[key] = $this.data('value');
-            $this.parent().parent().children('.dropdown-toggle').text($this.text());
+            App.update_value($(this), key);
         });
     }
     App.start = function() {
@@ -24,18 +27,38 @@
         this.hook(this.time_dropdown, 'time');
         this.hook(this.duration_dropdown, 'duration');
         this.hook(this.room_type_dropdown, 'room_type');
+
+        $(this.submit_button).click(this.handle_form_submit.bind(App));
+        $(this.building_input).keydown(function(event) {
+            if (event.key === 'Enter') {
+                App.handle_form_submit.call(App, undefined);
+            }
+        });
+        const [curr_day_of_week, curr_timeslot] = this.get_current_time();
+        $(this.day_dropdown).find('.dropdown-menu > .dropdown-item:nth-child('+(curr_day_of_week+1)+')').each(function() {
+            App.update_value($(this), 'day');
+        });
+        $(this.time_dropdown).find('.dropdown-menu > .dropdown-item:nth-child('+(curr_timeslot+1)+')').each(function() {
+            App.update_value($(this), 'time');
+        });
+        $(this.duration_dropdown).find('.dropdown-menu > .dropdown-item[data-default]').each(function() {
+            App.update_value($(this), 'duration');
+        });
+        $(this.room_type_dropdown).find('.dropdown-menu > .dropdown-item[data-default]').each(function() {
+            App.update_value($(this), 'room_type');
+        });
     }
     App.get_locations = function(listing) {
-        let result = new Set();
-        let process_sections = function (sections) {
-            for (let section of sections) {
-                for (let time of section.times) {
+        const result = new Set();
+        const process_sections = function (sections) {
+            for (const section of sections) {
+                for (const time of section.times) {
                     result.add(time[time.length - 1]);
                 }
             }
         };
-        for (let department in listing.courses) {
-            for (let course of listing.courses[department]) {
+        for (const department in listing.courses) {
+            for (const course of listing.courses[department]) {
                 if (course.hasOwnProperty('core')) {
                     process_sections(course.core);
                 }
@@ -69,9 +92,9 @@
     }
 
     App.get_current_time = function() {
-        let today = new Date();
-        let current_time = [today.getHours(), today.getMinutes()];
-        let timeslots = [
+        const today = new Date();
+        const current_time = [today.getHours(), today.getMinutes()];
+        const timeslots = [
             [[8, 30], [9, 30]],
             [[9, 30], [10, 30]],
             [[10, 30], [11, 30]],
@@ -86,18 +109,18 @@
         ];
         let chosen_slot = -1;
         let next_day = false;
-        if (time_le(current_time, timeslots[0][0])) {
+        if (this.time_le(current_time, timeslots[0][0])) {
             // before first
             chosen_slot = 0;
-        } else if (time_le(timeslots[timeslots.length - 1][1], current_time)) {
+        } else if (this.time_le(timeslots[timeslots.length - 1][1], current_time)) {
             // after last
             chosen_slot = 0;
             next_day = true;
         } else {
-            for (let i = 0; i < timeslots.length; i++) {
-                let last_iter = i === timeslots.length - 1;
-                if (time_le(timeslots[i][0], current_time) && (last_iter || time_le(current_time, timeslots[i][1]))) {
-                    if (time_sub(timeslots[i][1], current_time) > 20) {
+            for (const i = 0; i < timeslots.length; i++) {
+                const last_iter = i === timeslots.length - 1;
+                if (this.time_le(timeslots[i][0], current_time) && (last_iter || this.time_le(current_time, timeslots[i][1]))) {
+                    if (this.time_sub(timeslots[i][1], current_time) > 20) {
                         // more than 20m from end
                         chosen_slot = i;
                     } else if (last_iter) {
@@ -120,25 +143,25 @@
         return 60 * (time1[0] - time2[0]) + (time1[1] - time2[1]);
     }
 
-    App.time_print= function(time) {
+    App.time_print = function(time) {
         return time[0] + ':' + time[1];
     }
 
     App.find_current_lectures = function(listing, day_of_week, start_time, end_time) {
-        let result = [];
-        for (let department in listing.courses) {
-            for (let course of listing.courses[department]) {
+        const result = [];
+        for (const department in listing.courses) {
+            for (const course of listing.courses[department]) {
                 if (course.term !== 1) {
                     continue;
                 }
                 if (course.hasOwnProperty('core')) {
-                    for (let section of course.core) {
-                        for (let timeslot of section.times) {
+                    for (const section of course.core) {
+                        for (const timeslot of section.times) {
                             if (timeslot.length > 1) {
-                                let [time, location] = decode_timeslot(timeslot);
+                                const [time, location] = this.decode_timeslot(timeslot);
                                 if (time.day_of_week === day_of_week
-                                    && time_le(time.start_time, end_time)
-                                    && time_le(start_time, time.end_time)) {
+                                    && this.time_le(time.start_time, end_time)
+                                    && this.time_le(start_time, time.end_time)) {
                                     result.push({
                                         start_time: time.start_time,
                                         end_time: time.end_time,
@@ -156,23 +179,23 @@
     }
 
     App.find_unoccupied = function(listing, day_of_week, start_time, end_time) {
-        let locations = get_locations(listing);
-        let process_sections = function (sections) {
-            for (let section of sections) {
-                for (let timeslot of section.times) {
+        const locations = this.get_locations(listing);
+        const process_sections = function (sections) {
+            for (const section of sections) {
+                for (const timeslot of section.times) {
                     if (timeslot.length > 1) {
-                        let [time, location] = decode_timeslot(timeslot);
+                        const [time, location] = App.decode_timeslot(timeslot);
                         if (time.day_of_week === day_of_week
-                            && time_le(time.start_time, end_time)
-                            && time_le(start_time, time.end_time)) {
+                            && App.time_le(time.start_time, end_time)
+                            && App.time_le(start_time, time.end_time)) {
                             locations.delete(location);
                         }
                     }
                 }
             }
         };
-        for (let department in listing.courses) {
-            for (let course of listing.courses[department]) {
+        for (const department in listing.courses) {
+            for (const course of listing.courses[department]) {
                 if (course.term !== 1) {
                     continue;
                 }
@@ -191,61 +214,41 @@
     }
 
     App.handle_form_submit = function(event) {
-        let building = document.getElementById('input-building').value.toUpperCase();
-        let day_of_week = document.getElementById('input-day').value;
-        let start_time = document.getElementById('input-time-start').value.split(':').map(Number);
-        let time_length = Number(document.getElementById('input-time-length').value);
-        let end_time = [start_time[0] + time_length, start_time[1] - 1];
+        const building = $(this.building_input).val().toUpperCase();
+        const day_of_week = this.day;
+        const start_time = this.time.split(':').map(Number);
+        const time_length = Number(this.duration);
+        const end_time = [start_time[0] + time_length, start_time[1] - 1];
 
         let filtered = [];
-        let empty_only_selector = document.getElementById('input-empty-only');
-        if (empty_only_selector.value === 'Empty') {
-            let unoccupied = Array.from(find_unoccupied(listing, day_of_week, start_time, end_time));
+        if (this.room_type === 'Empty') {
+            const unoccupied = Array.from(this.find_unoccupied(listing, day_of_week, start_time, end_time));
             filtered = unoccupied.filter(loc => loc.startsWith(building));
-        } else if (empty_only_selector.value === 'All') {
-            let curr_lectures = find_current_lectures(listing, day_of_week, start_time, end_time)
+        } else if (this.room_type === 'All') {
+            const curr_lectures = this.find_current_lectures(listing, day_of_week, start_time, end_time)
                 .filter(obj => obj.room.startsWith(building))
-                .map(obj => `${obj.room}<br>
-                         ${obj.course_name}<br>
-                         ${time_print(obj.start_time)}-${time_print(obj.end_time)}`);
-            let unoccupied = Array.from(find_unoccupied(listing, day_of_week, start_time, end_time))
+                .map(obj => `${obj.room}
+                         <span class="cname">${obj.course_name}</span>
+                         <span class="time">${this.time_print(obj.start_time)}-${this.time_print(obj.end_time)}</span>`);
+            const unoccupied = Array.from(this.find_unoccupied(listing, day_of_week, start_time, end_time))
                 .filter(loc => loc.startsWith(building));
             filtered = curr_lectures.concat(unoccupied);
             filtered = Array.from(new Set(filtered));
         } else {
-            throw 'Unexpected selection value: ' + empty_only_selector.value;
+            throw 'Unexpected selection value: ' + this.room_type;
         }
 
         filtered.sort();
 
-        let output_div = document.getElementById('request-output');
-        output_div.innerHTML = '';
-        for (let room of filtered) {
-            let room_div = document.createElement('div');
-            room_div.className = 'output-item';
-            room_div.innerHTML = room;
-            output_div.appendChild(room_div);
+        $(this.output_container).empty();
+        for (const room of filtered) {
+            $(this.output_container).append(
+                $(`<li class="list-group-item">${room}</li>`)
+            );
         }
-        //output_div.scrollIntoView({behavior: 'smooth', block: 'top'});
-        window.scroll({
-            top: output_div.getBoundingClientRect().top - 5,
-            behavior: 'smooth'
-        });
     }
     window.App = App;
     window.addEventListener('load', function() {
         window.App.start();
-        // let input_form = document.getElementById('submit-data');
-        // input_form.addEventListener('click', handle_form_submit, false);
-        // let input_building = document.getElementById('input-building');
-        // input_building.onkeydown = function (event) {
-        //     if (event.key === 'Enter') {
-        //         handle_form_submit(undefined);
-        //     }
-        // };
-        // let [curr_day_of_week, curr_timeslot] = get_current_time();
-        // document.getElementById('input-day').selectedIndex = curr_day_of_week;
-        // document.getElementById('input-time-start').selectedIndex = curr_timeslot;
-
-    });
+    })
 })();
